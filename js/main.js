03f701eb9553a4f926a84a241f88b2f20fc3bc6b
Alex <AlexGurDev@gmail.com>
2023-05-31 21:27:29 +0300

    add mobile header

--- js/main.js
+++ js/main.js
@@ -1,28 +1,20 @@
-// Nav-icon
-
 const navBtn = document.querySelector('.nav-icon-btn');
 const navIcon = document.querySelector('.nav-icon');
 const nav = document.querySelector('.header__inner-wrapper');
+const select = document.querySelector('.select-box__selected');
+const selectList = document.querySelector('.select-box__list');
+const slider = document.querySelector(".range-slider__input");
+const output = document.querySelector(".range-slider__output-number");
 
 navBtn.addEventListener('click', () => {
 	navIcon.classList.toggle('nav-icon--active');
 	nav.classList.toggle('header__inner-wrapper--mobile');
 });
 
-const select = document.querySelector('.select-box__selected');
-const selectList = document.querySelector('.select-box__list');
-
 select.addEventListener('click', () => {
 	selectList.classList.toggle('open');
 	select.classList.toggle('select-box__selected--open');
 });
 
-
-
-var slider = document.querySelector(".range-slider__input");
-var output = document.querySelector(".range-slider__output-number");
 output.innerHTML = slider.value;
-
-slider.oninput = function () {
-	output.innerHTML = this.value;
-};
+slider.addEventListener('input', () => output.innerHTML = slider.value);
