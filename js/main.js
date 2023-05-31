66d76bc29b7171537c9faa369e586bff43bc5fb2
Alex <AlexGurDev@gmail.com>
2023-06-01 00:41:17 +0300

    fix div to footer

--- js/main.js
+++ js/main.js
@@ -1,17 +1,1 @@
-const navBtn = document.querySelector('.nav-icon-btn');
-const navIcon = document.querySelector('.nav-icon');
-const nav = document.querySelector('.header__inner-wrapper');
-const select = document.querySelector('.select-box__selected');
-const selectList = document.querySelector('.select-box__list');
-const slider = document.querySelector(".range-slider__input");
-const output = document.querySelector(".range-slider__output-number");
-navBtn.addEventListener('click', () => {
-	navIcon.classList.toggle('nav-icon--active');
-	nav.classList.toggle('header__inner-wrapper--mobile');
-});
-select.addEventListener('click', () => {
-	selectList.classList.toggle('open');
-	select.classList.toggle('select-box__selected--open');
-});
-output.innerHTML = slider.value;
-slider.addEventListener('input', () => output.innerHTML = slider.value);
+const navBtn=document.querySelector('.nav-icon-btn');const navIcon=document.querySelector('.nav-icon');const nav=document.querySelector('.header__inner-wrapper');const select=document.querySelector('.select-box__selected');const selectList=document.querySelector('.select-box__list');const slider=document.querySelector(".range-slider__input");const output=document.querySelector(".range-slider__output-number");navBtn.addEventListener('click',()=>{navIcon.classList.toggle('nav-icon--active');nav.classList.toggle('header__inner-wrapper--mobile');});select.addEventListener('click',()=>{selectList.classList.toggle('open');select.classList.toggle('select-box__selected--open');});output.innerHTML=slider.value;slider.addEventListener('input',()=>output.innerHTML=slider.value);
